9668adc558a49d26db58ef856268de7cff3562a9
94buhuicode <sentianer@gmail.com>
2023-10-18 12:06:58 +0800

    initialize npm package

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+​
+// Port to listen to
+const PORT = parseInt(process.env.PORT) || 3000
+​
+// Create an instance of express
+const app = express()
+
+// Serve files in the static directory
+app.use(express.static(__dirname + '/static'))
+​
+app.listen(PORT, () => {
+   console.info(`Application started on port ${PORT} at ${new Date()}`)
+})
